5b99f4a2acfc32ee5deec9aae3d6a0fcbfc4abf9
samlm0 <admin@need.sh>
2021-08-15 23:06:24 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# enable-bbr
+# Enable bbr kernel featrue via systemd
+
+# How to use
+Just copy and paste to your shell, Press [enter] and the config will be automatically deploy
+
+```
+sh -c "$(wget -qO - https://raw.githubusercontent.com/wikihost-opensource/enable-bbr/main/run.sh)"
+```
+
+# TODO
+- [ ] CentOS 7 Auto install support kernel
+- [ ] Debian 9 Auto install support kernel
---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Backup your old config
+OLD_QDISC=$(cat /proc/sys/net/core/default_qdisc)
+OLD_TCP_CONTROL=$(cat /proc/sys/net/ipv4/tcp_congestion_control)
+
+_rollback(){
+    echo $OLD_QDISC > /proc/sys/net/core/default_qdisc
+    echo $OLD_TCP_CONTROL > /proc/sys/net/ipv4/tcp_congestion_control
+}
+
+rollback(){
+    _rollback
+    if [ -z "$1" ];
+        echo "Failed: Current kernel did not support this feature"
+    fi;
+    exit 1
+}
+
+
+echo 'bbr' > /proc/sys/net/ipv4/tcp_congestion_control
+if [ $? -ne 0 ];then
+    rollback
+fi;
+echo "Great, kernel support bbr feature, downloading systemd service to your system..."
+_rollback
+wget -O /etc/systemd/system/kernel-bbr.service https://raw.githubusercontent.com/wikihost-opensource/enable-bbr/main/kernel-bbr.service
+systemctl start kernel-bbr > /dev/null
+if [ `systemctl is-active kernel-bbr.service` == "active" ];then
+    systemctl enable kernel-bbr > /dev/null
+    echo "Success, bbr kernel feature is enabled"
+    exit 0
+fi;
+
+rollback
